ae0a27931452a0dd6e0512203a8f66016297ad0b
Felipe Tramontin <felipetramontin2008@gmail.com>
2026-04-05 20:26:48 -0400

    created middleware folder and coded files with proper basic middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,9 @@
+module.exports = (err, req, res, next) => {
+    console.error('🔥 Error:', err.message);
+
+    const status = err.status || 500;
+
+    res.status(status).json({
+        error: err.message || 'Internal Server Error'
+    });
+};
