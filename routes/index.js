2bc42aff14ea518fe2c0ebd8dd2f9b2fcd37f246
Felipe Tramontin <felipetramontin2008@gmail.com>
2026-04-24 03:57:13 -0400

    updated index route to fit the auth route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
+// routes/index.js
 const express = require('express');
 const router = express.Router();
 
+router.use('/auth', require('./auth'));
 router.use('/users', require('./users'));
 router.use('/sessions', require('./sessions'));
 router.use('/exercises', require('./exercises'));
